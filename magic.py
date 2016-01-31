7bc7898093e7c3279e8a276f217f79a521830538
deerishi <deerishi@gmail.com>
2016-01-31 13:09:12 -0500

    Files for magic.py

--- magic.py
+++ magic.py
@@ -1,49 +1,23 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import selenium
 from selenium.webdriver.common.keys import Keys
 import time
 driver=webdriver.Firefox()
 driver.get("http://www.google.com")
 inputelement=driver.find_element_by_name("q")
-inputelement.send_keys("University of Waterloo Python Workshop mentors")
-inputelement.submit()
-button=driver.find_elements_by_class_name("searchbutton")
-inputelement.send_keys(Keys.RETURN)
-time.sleep(5)
-inputelement.clear()
-inputelement.send_keys(" Ivana Kajic")
-inputelement.submit()
-time.sleep(4)
-inputelement.clear()
-inputelement.send_keys("Sean Aubin")
-inputelement.submit()
-time.sleep(5)
-inputelement.clear()
-inputelement.send_keys("Alan Tsang University of Waterloo")
-inputelement.submit()
-time.sleep(5)
-inputelement.clear()
-inputelement.send_keys("Sajed Haque University of Waterloo")
-inputelement.submit()
-time.sleep(5)
-inputelement.clear()
-inputelement.send_keys(" Mariah Martin Shein University of Waterloo")
-inputelement.submit()
-time.sleep(5)
-inputelement.clear()
-inputelement.send_keys(" Irish Medina University of Waterloo")
-inputelement.submit()
-time.sleep(5)
-inputelement.clear()
-inputelement.send_keys(" Deepak Rishi BITS")
-inputelement.submit()
+
+f=open('names.txt','r')
+names=[]
+for line in f:
+    names.append(line.split('\n')[0])
+count=0
+for name in names:
+    inputelement.send_keys(name)
+    inputelement.submit()
+    time.sleep(5)
+    count=count+1
+    if count<len(names):
+        inputelement.clear()
 time.sleep(10)
-
-
 driver.quit()
-
-    
-
