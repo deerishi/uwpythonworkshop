66205826e7f014c955dd42db2757c157cada9e3e
Deepak Rishi <deerishi@gmail.com>
2017-11-18 23:45:49 -0500

    adding all

--- magic.py
+++ magic.py
@@ -3,8 +3,10 @@ from selenium.common.exceptions import TimeoutException
 import selenium
 from selenium.webdriver.common.keys import Keys
 import time
-
-driver = webdriver.Firefox(executable_path='/home/drishi/CoolStuffinPython/geckodriver')
+from selenium.webdriver.chrome.options import Options
+_chrome_options = Options()
+_chrome_options.add_argument('disable-infobars')
+driver = webdriver.Chrome(executable_path='/home/drishi/CoolStuffinPython/chromedriver',chrome_options=_chrome_options)
 
 #driver=webdriver.Firefox()
 driver.get("http://www.google.com")
@@ -16,11 +18,14 @@ for line in f:
     names.append(line.split('\n')[0])
 count=0
 for name in names:
+    print 'Name is ',name
+    inputelement=driver.find_element_by_name("q")
     inputelement.send_keys(name)
     inputelement.submit()
     time.sleep(5)
     count=count+1
     if count<len(names):
+        inputelement=driver.find_element_by_name("q")
         inputelement.clear()
 time.sleep(10)
 driver.quit()
